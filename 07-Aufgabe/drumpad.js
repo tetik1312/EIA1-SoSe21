c341248ab63b7bafa626e88dc661b8894f9383e5
Unknown <tetik99@gmx.de>
2021-05-07 14:33:52 +0200

    Aufgabe 7 fertig

--- 07-Aufgabe/drumpad.js
+++ 07-Aufgabe/drumpad.js
@@ -2,6 +2,8 @@ function playSample(sounds) {
     var sound = new Audio(sounds);
     sound.play();
 }
+var beats = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3"];
+var index = 0;
 document.querySelector('.button1').addEventListener('click', function () { playSample("assets/A.mp3"); });
 document.querySelector('.button2').addEventListener('click', function () { playSample("assets/C.mp3"); });
 document.querySelector('.button3').addEventListener('click', function () { playSample("assets/F.mp3"); });
@@ -11,15 +13,14 @@ document.querySelector('.button6').addEventListener('click', function () { playS
 document.querySelector('.button7').addEventListener('click', function () { playSample("assets/snare.mp3"); });
 document.querySelector('.button8').addEventListener('click', function () { playSample("assets/laugh-1.mp3"); });
 document.querySelector('.button9').addEventListener('click', function () { playSample("assets/laugh-2.mp3"); });
-document.querySelector('.playButton').addEventListener('click', function () { setInterval("assets/hihat.mp3"); });
-var beats = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3"];
-var index = 0;
-setInterval(function (beats) {
-    var beat = new Audio(beats[index]);
-    beat.play();
-    index = index + 1;
-    //if(index == 3){
-    //index = 0;
-    //}
-}, 500);
+document.querySelector('.playButton').addEventListener('click', function () {
+    var interval = setInterval(function () {
+        var beat = new Audio(beats[index]);
+        beat.play();
+        index = index + 1;
+        if (index == 3) {
+            index = 0;
+        }
+    }, 500);
+});
 //# sourceMappingURL=drumpad.js.map
