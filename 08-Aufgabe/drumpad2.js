f6fd824de6ff617ac3c5dfab089cab48b2bb6a9f
Unknown <tetik99@gmx.de>
2021-05-14 15:53:35 +0200

    aufgabe 8 mit remix button

--- 08-Aufgabe/drumpad2.js
+++ 08-Aufgabe/drumpad2.js
@@ -6,6 +6,7 @@ var Aufgabe8;
     }
     var beats = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3"];
     var index = 0;
+    var interval;
     document.querySelector(".button1").addEventListener("click", function () { playSample("assets/A.mp3"); });
     document.querySelector(".button2").addEventListener("click", function () { playSample("assets/C.mp3"); });
     document.querySelector(".button3").addEventListener("click", function () { playSample("assets/F.mp3"); });
@@ -16,48 +17,56 @@ var Aufgabe8;
     document.querySelector(".button8").addEventListener("click", function () { playSample("assets/laugh-1.mp3"); });
     document.querySelector(".button9").addEventListener("click", function () { playSample("assets/laugh-2.mp3"); });
     document.querySelector("#play").addEventListener("click", function () {
-        var interval = setInterval(function () {
+        interval = setInterval(function () {
             playSample(beats[index]);
             index = index + 1;
-            if (index == 3) {
+            if (index == beats.length) {
                 index = 0;
             }
         }, 500);
     });
-})(Aufgabe8 || (Aufgabe8 = {}));
-document.querySelector("#remix").addEventListener("click", function () {
-    function remixPlay(beats) {
-        beats: [];
-        for (var index_1 = 0; index_1 < 2; index_1++) {
-            var beats = Math.random().toString();
+    var sounds = ["assets/A.mp3", "assets/C.mp3", "assets/F.mp3", "assets/G.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3"];
+    document.querySelector("#remix").addEventListener("click", function () { remixPlay(); });
+    function remixPlay() {
+        deleteButton();
+        for (var index_1 = 0; index_1 <= 4; index_1++) {
+            var random = Math.floor(Math.random() * 8);
+            beats[index_1] = sounds[random];
+            console.log(random);
         }
+        interval = setInterval(function () {
+            playSample(beats[index]);
+            index = index + 1;
+            if (index == beats.length) {
+                index = 0;
+            }
+        }, 500);
     }
-});
-document.querySelector("#delete").addEventListener("click", deleteButton);
-function deleteButton() {
-    beats.length = 0;
-}
-document.querySelector("#pause").addEventListener("click", myStopFunction);
-function myStopFunction() {
-    playSample(beats[index]);
-    var sound;
-    sound.pause();
-}
-//variablen angelegt
-var myPlay = document.getElementById("play");
-var myPause = document.getElementById("pause");
-//wenn auf play geklickt wird, wird die funktion toggleicons aufgerufen
-myPlay.addEventListener("click", function () {
-    toggleIcons(this, myPause);
-});
-//wenn auf pause geklickt wird, wird die funktion toggleicons aufgerufen
-myPause.addEventListener("click", function () {
-    toggleIcons(this, myPlay);
-});
-//uebergebe zwei elemente, mit dem ersten element wird die klasse 'is-hidden' geaddet und ist nich mehr sichtbar
-//beim zweiten element wird die klasse 'is-hidden' removed und ist wieder sichtbar
-function toggleIcons(firstHTMLElement, secondHTMLElement) {
-    firstHTMLElement.classList.add("is-hidden");
-    secondHTMLElement.classList.remove("is-hidden");
-}
+    document.querySelector("#delete").addEventListener("click", function () { deleteButton(); });
+    function deleteButton() {
+        beats.length = 0;
+        myStopFunction();
+    }
+    document.querySelector("#pause").addEventListener("click", myStopFunction);
+    function myStopFunction() {
+        clearInterval(interval);
+    }
+    //variablen angelegt
+    var myPlay = document.getElementById("play");
+    var myPause = document.getElementById("pause");
+    //wenn auf play geklickt wird, wird die funktion toggleicons aufgerufen
+    myPlay.addEventListener("click", function () {
+        toggleIcons(this, myPause);
+    });
+    //wenn auf pause geklickt wird, wird die funktion toggleicons aufgerufen
+    myPause.addEventListener("click", function () {
+        toggleIcons(this, myPlay);
+    });
+    //uebergebe zwei elemente, mit dem ersten element wird die klasse 'is-hidden' geaddet und ist nich mehr sichtbar
+    //beim zweiten element wird die klasse 'is-hidden' removed und ist wieder sichtbar
+    function toggleIcons(firstHTMLElement, secondHTMLElement) {
+        firstHTMLElement.classList.add("is-hidden");
+        secondHTMLElement.classList.remove("is-hidden");
+    }
+})(Aufgabe8 || (Aufgabe8 = {}));
 //# sourceMappingURL=drumpad2.js.map
--- 08-Aufgabe/drumpad2.ts
+++ 08-Aufgabe/drumpad2.ts
@@ -7,6 +7,7 @@ namespace Aufgabe8 {
     var beats: string[] = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3"];
 
     var index: number = 0;
+    var interval: number;
 
     document.querySelector(".button1").addEventListener("click", function (): void { playSample("assets/A.mp3"); });
     document.querySelector(".button2").addEventListener("click", function (): void { playSample("assets/C.mp3"); });
@@ -19,65 +20,80 @@ namespace Aufgabe8 {
     document.querySelector(".button9").addEventListener("click", function (): void { playSample("assets/laugh-2.mp3"); });
     document.querySelector("#play").addEventListener("click", function (): void {
 
-        var interval: number = setInterval(function (): void {
+        interval = setInterval(function (): void {
             playSample(beats[index]);
 
             index = index + 1;
 
-            if (index == 3) {
+            if (index == beats.length) {
                 index = 0;
 
             }
 
         }, 500);
     });
-}
 
 
+    var sounds: string[] = ["assets/A.mp3", "assets/C.mp3", "assets/F.mp3", "assets/G.mp3", "assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3" ];
+ 
 
-document.querySelector("#remix").addEventListener("click", function (): void {
 
-    function remixPlay(beats: string): void {
-        beats: [] = [];
-        for (let index: number = 0; index < 2; index++) {
-            var beats: string = Math.random().toString();
+    document.querySelector("#remix").addEventListener("click", function (): void { remixPlay(); });
+
+    function remixPlay(): void {
+        deleteButton();
+        for (let index: number = 0; index <= 4; index++) {
+            var random: number = Math.floor(Math.random() * 8);
+            beats[index] = sounds[random];
+            console.log(random);
         }
+        interval = setInterval(function (): void {
+            playSample(beats[index]);
+
+            index = index + 1;
+
+            if (index == beats.length) {
+                index = 0;
+            }
+
+        }, 500);
+    }
+
+
+    document.querySelector("#delete").addEventListener("click", function (): void { deleteButton(); });
+
+    function deleteButton(): void {
+        beats.length = 0;
+        myStopFunction();
+    }
+
+    document.querySelector("#pause").addEventListener("click", myStopFunction);
+
+    function myStopFunction(): void {
+        clearInterval(interval);
 
     }
-});
-
-document.querySelector("#delete").addEventListener("click", deleteButton);
-
-function deleteButton(): void {
-    beats.length = 0;
-}
-
-document.querySelector("#pause").addEventListener("click", myStopFunction);
-
-function myStopFunction(): void {
-    playSample(beats[index]);
-    var sound: HTMLAudioElement;
-    sound.pause();
-}
 
 
 
-//variablen angelegt
-var myPlay: HTMLElement = document.getElementById("play");
-var myPause: HTMLElement = document.getElementById("pause");
+    //variablen angelegt
+    var myPlay: HTMLElement = document.getElementById("play");
+    var myPause: HTMLElement = document.getElementById("pause");
 
-//wenn auf play geklickt wird, wird die funktion toggleicons aufgerufen
-myPlay.addEventListener("click", function (): void {
-    toggleIcons(this, myPause);
-});
-//wenn auf pause geklickt wird, wird die funktion toggleicons aufgerufen
-myPause.addEventListener("click", function (): void {
-    toggleIcons(this, myPlay);
-});
+    //wenn auf play geklickt wird, wird die funktion toggleicons aufgerufen
+    myPlay.addEventListener("click", function (): void {
+        toggleIcons(this, myPause);
+    });
+    //wenn auf pause geklickt wird, wird die funktion toggleicons aufgerufen
+    myPause.addEventListener("click", function (): void {
+        toggleIcons(this, myPlay);
+    });
 
-//uebergebe zwei elemente, mit dem ersten element wird die klasse 'is-hidden' geaddet und ist nich mehr sichtbar
-//beim zweiten element wird die klasse 'is-hidden' removed und ist wieder sichtbar
-function toggleIcons(firstHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void {
-    firstHTMLElement.classList.add("is-hidden");
-    secondHTMLElement.classList.remove("is-hidden");
+    //uebergebe zwei elemente, mit dem ersten element wird die klasse 'is-hidden' geaddet und ist nich mehr sichtbar
+    //beim zweiten element wird die klasse 'is-hidden' removed und ist wieder sichtbar
+    function toggleIcons(firstHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void {
+        firstHTMLElement.classList.add("is-hidden");
+        secondHTMLElement.classList.remove("is-hidden");
+    }
+
 }
